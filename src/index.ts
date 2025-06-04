413308d4f77d4ff864a63bfcc29dc022d66e9aad
hasanemam1717 <hasanemam1717@gmail.com>
2025-06-04 15:22:50 +0600

    learn how create data in prisma

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "../generated/prisma";
+
+const prisma = new PrismaClient()
+
+const main = async () => {
+    // create data in prisma
+    // const result = await prisma.post.create({
+    //     data: {
+    //         title: "This is the prisma project title2",
+    //         content: "This is content2",
+    //         author: "Hasan Emam"
+    //     }
+    // })
+    // console.log(result);
+
+
+    // get data in prisma
+}
+
+main()
